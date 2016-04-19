29e5fe5d9aafb27ef3890733c8d61b14454dc8ca
Joshua Hunsberger <joshua@hunsberger.co>
2016-04-18 21:05:22 -0400

    Replace default ViewController with TravelMapViewController

---
+++ VirtualTourist/TravelMapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TravelMapViewController.swift
+//  VirtualTourist
+//
+//  Created by Joshua Hunsberger on 4/18/16.
+//  Copyright © 2016 Joshua Hunsberger. All rights reserved.
+//
+
+import UIKit
+
+class TravelMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
