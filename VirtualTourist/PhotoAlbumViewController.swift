29d5feb6c9a0c6fc0b647ce3add6d2b70aaf34d9
Joshua Hunsberger <joshua@hunsberger.co>
2016-04-19 23:56:15 -0400

    feat: Add PhotoAlbumViewController
    Add a custom view controller for the photo album view
    Set the view controller for the view in storyboard
    Create IBOutlets from storyboard to the view controller

---
+++ VirtualTourist/PhotoAlbumViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Joshua Hunsberger on 4/19/16.
+//  Copyright © 2016 Joshua Hunsberger. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class PhotoAlbumViewController: UIViewController {
+    
+    // MARK: InterfaceBuilder outlet properties
+    
+    @IBOutlet weak var pinMapView: MKMapView!
+    @IBOutlet weak var photoCollectionView: UICollectionView!
+    @IBOutlet weak var photoUpdateButton: UIButton!
+    
+    // MARK: View lifecycle functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
