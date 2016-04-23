aba5ee2cb5b2acc2f007832721c47815a94e5233
Joshua Hunsberger <joshua@hunsberger.co>
2016-04-23 18:52:44 -0400

    feat: Add class to interact with Flickr
    FlickrClient will be used to interact with Flickr.  The class has a sharedInstance property
    that makes it accessible as a Singleton.

---
+++ VirtualTourist/FlickrClient.swift
@@ -0,0 +1,16 @@
+//
+//  FlickrClient.swift
+//  VirtualTourist
+//
+//  Created by Joshua Hunsberger on 4/23/16.
+//  Copyright © 2016 Joshua Hunsberger. All rights reserved.
+//
+
+import Foundation
+
+class FlickrClient: NSObject {
+    
+    // Make FlickrClient a singleton with a single line.
+    // From http://krakendev.io/blog/the-right-way-to-write-a-singleton
+    static let sharedInstance = FlickrClient()
+}
