94c6e4de5e33151736bc6a2d9e70a86dc0786e6d
Joshua Hunsberger <joshua@hunsberger.co>
2016-04-24 10:52:51 -0400

    feat: Add function to generate Flickr URL
    Add a helper function to generate a URL for querying Flickr
    given a set of URL parameters.

--- VirtualTourist/FlickrClient.swift
+++ VirtualTourist/FlickrClient.swift
@@ -13,4 +13,21 @@ class FlickrClient: NSObject {
     // Make FlickrClient a singleton with a single line.
     // From http://krakendev.io/blog/the-right-way-to-write-a-singleton
     static let sharedInstance = FlickrClient()
+    
+    // MARK: URL Helper Function
+    
+    func createFlickrURLFromParameters(parameters: [String: AnyObject]) -> NSURL {
+        let components = NSURLComponents()
+        components.scheme = Constants.APIScheme
+        components.host = Constants.APIHost
+        components.path = Constants.APIPath
+        components.queryItems = [NSURLQueryItem]()
+        
+        for (key, value) in parameters {
+            let queryItem = NSURLQueryItem(name: key, value: "\(value)")
+            components.queryItems!.append(queryItem)
+        }
+        
+        return components.URL!
+    }
 }
