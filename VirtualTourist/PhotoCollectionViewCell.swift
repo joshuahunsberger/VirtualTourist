66bc6c779fef8ded4dacaf86662d9b6165adb048
Joshua Hunsberger <joshua@hunsberger.co>
2016-04-24 21:02:51 -0400

    feat: Create custom collection cell
    Create a custom collection view cell for displaying Flickr photos.

---
+++ VirtualTourist/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  VirtualTourist
+//
+//  Created by Joshua Hunsberger on 4/24/16.
+//  Copyright © 2016 Joshua Hunsberger. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoImageView: UIImageView!
+}
