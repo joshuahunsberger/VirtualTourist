ee9f6549a1e6cc3cd7d73471aae7f3a4fc04c46f
Joshua Hunsberger <joshua@hunsberger.co>
2016-05-02 22:47:26 -0400

    feat: Implement CoreDataStackManager
    Create a CoreDataStackManager class in the pattern of the Udacity
    FavoriteActors class.

---
+++ VirtualTourist/CoreDataStackManager.swift
@@ -0,0 +1,77 @@
+//
+//  CoreDataStackManager.swift
+//  VirtualTourist
+//
+//  Created by Joshua Hunsberger on 4/26/16.
+//  Copyright © 2016 Joshua Hunsberger. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStackManager {
+    
+    // MARK: Properties
+    let sharedManager = CoreDataStackManager()
+    private let SQLITE_FILE_NAME = "VirtualTourist.sqlite"
+    
+    //TODO: Finish implementing stack manager
+    
+    // MARK: Core Data stack
+    
+    lazy var applicationDocumentsDirectory: NSURL = {
+       let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        return urls.last!
+    }()
+    
+    /// The managed object model for the application
+    lazy var managedObjectModel: NSManagedObjectModel = {
+        let modelURL = NSBundle.mainBundle().URLForResource("Model", withExtension: "momd")!
+        
+        return NSManagedObjectModel(contentsOfURL: modelURL)!
+    }()
+    
+    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
+       let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent(self.SQLITE_FILE_NAME)
+        
+        do {
+            try persistentStoreCoordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
+        } catch {
+            var dict = [String: AnyObject]()
+            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
+            dict[NSLocalizedFailureReasonErrorKey] = "There was an error creating or loading the application's saved data."
+            
+            dict[NSUnderlyingErrorKey] = error as NSError
+            let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+            // Replace this with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
+            abort()
+        }
+        
+        return persistentStoreCoordinator
+    }()
+    
+    lazy var managedObjectContext: NSManagedObjectContext = {
+       let coordinator = self.persistentStoreCoordinator
+        var managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = coordinator
+        return managedObjectContext
+    }()
+    
+    func saveContext() {
+        if managedObjectContext.hasChanges {
+            do {
+                try managedObjectContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+                abort()
+            }
+        }
+        
+    }
+}
